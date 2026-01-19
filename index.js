36097a4dbecc2ea0c2cd93a190d66a3007a308b6
Isabel Lin <isabel.lin@sptech.school>
2026-01-19 17:32:52 -0300

    Atualizando CSS da tabela

--- index.js
+++ index.js
@@ -3,28 +3,31 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-import { pool } from "./src/database/db.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import open from "open";
+
 import router from "./routes/estoqueRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'src')));
 
-
-app.get("/", async (req, res) => {
-    try {
-        res.send("Hello express!");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Erro");
-    }
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
 app.use("/api", router);
 
 app.listen(PORT, () => {
+    const url = `http://localhost:${PORT}`;
     console.log(`Server is running on port ${PORT}`);
+    open(url);
 })
--- models/estoqueModel.js
+++ models/estoqueModel.js
@@ -1,4 +1,4 @@
-import { pool } from "../src/database/db.js";
+import { pool } from "../database/db.js";
 
 class estoqueModel {
     async listarEstoque() {
